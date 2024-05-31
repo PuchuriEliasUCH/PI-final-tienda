91dbd224dbd75f9e4395d06d22d0ef5e948d87f9
PuchuriEliasUCH <elipuchuries@uch.pe>
2024-05-31 00:56:13 -0500

    [Fix] -> functional app

--- utils/cnx.py
+++ utils/cnx.py
@@ -1,23 +1,55 @@
 import mysql.connector
 
 class Connection:
+    import mysql.connector
+
+class Connection:
     def __init__(self, config):
+        self.cnx = None
         self.cnx = mysql.connector.connect(**config)
-        self.cursor = self.cnx.cursor()
 
-    def query(self, query, params=None):
-        self.cursor.execute(query, params)
-        return self.cursor.fetchall()
-
-    def insert(self, query, params):
-        self.cursor.execute(query, params)
+    def ejecutar_consulta(self, query, args = ()):
+        cursor = self.cnx.cursor()
+        cursor.execute(query, args)
+        return cursor
+    
+    def insert(self, query, args = ()):
+        cursor = self.ejecutar_consulta(query, args)
         self.cnx.commit()
-        return "Insertado correctamente"
+        cursor.close()
+        return "Nuevo registro añadido correctamente"
 
-    def execute(self, query, params):
-        self.cursor.execute(query, params)
+    def select_by_id(self, query, args = ()):
+        res = None
+        cursor = self.ejecutar_consulta(query, args)
+        if cursor.with_rows:
+            res = cursor.fetchone()
+        cursor.close()
+        return res
+
+    def select_all(self, query, args = ()):
+        res = None
+        cursor = self.ejecutar_consulta(query, args)
+        if cursor.with_rows:
+            res = cursor.fetchall()
+        cursor.close()
+        return res
+
+    def update(self, query, args = ()):
+        cursor = self.ejecutar_consulta(query, args)
         self.cnx.commit()
+        cursor.close()
+        return "Registro actualizado correctamente"
 
-    def close(self):
-        self.cursor.close()
-        self.cnx.close()
+    def delete(self, query, args = ()):
+        cursor = self.ejecutar_consulta(query, args)
+        self.cnx.commit()
+        cursor.close()
+        return "Registo eliminado coerrectamente"
+    
+    # Reportes y consultas especificas
+
+    
+    def __del__(self):
+        if self.cnx != None:
+            self.cnx.close()
